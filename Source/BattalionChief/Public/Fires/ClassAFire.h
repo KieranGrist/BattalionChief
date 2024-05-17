bb7b0e41cc9879a579bd5dbcb46d609e26bf04f0
Kieran Grist <up814853@myport.ac.uk>
2024-05-17 13:44:12 +0100

    Fire Types

---
+++ Source/BattalionChief/Public/Fires/ClassAFire.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Fires/BaseFireActor.h"
+#include "ClassAFire.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTALIONCHIEF_API AClassAFire : public ABaseFireActor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BattalionChief/Public/Fires/ClassBFire.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Fires/BaseFireActor.h"
+#include "ClassBFire.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTALIONCHIEF_API AClassBFire : public ABaseFireActor
+{
+	GENERATED_BODY()
+	
+};
