c1197ed2634fcae0693b8ca3cacb7faddd158ea4
Kieran Grist <up814853@myport.ac.uk>
2024-05-17 13:30:30 +0100

    Extinguisher Types

---
+++ Source/BattalionChief/Public/ExtinguisherTypes/CarbonDioxide.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Equipment/FireExtinguisher.h"
+#include "CarbonDioxide.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTALIONCHIEF_API ACarbonDioxide : public AFireExtinguisher
+{
+	GENERATED_BODY()
+	
+};
